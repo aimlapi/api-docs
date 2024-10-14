c131c3455e284ecf483106105fe0c1496e347ed8
Daniil Bombenkov <danixoon@gmail.com>
2024-10-14 15:44:11 +0300

    fix: add the base url

--- snippet-app/vite.config.ts
+++ snippet-app/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://cdn.jsdelivr.net/gh/aimlapi/api-docs/snippet-app/dist',
   plugins: [react()],
   build: {
     rollupOptions: {
