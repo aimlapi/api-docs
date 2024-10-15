54f19802114c28ad4cdb853535bb1b9e68aa4621
Daniil Bombenkov <danixoon@gmail.com>
2024-10-15 18:17:05 +0300

    fix: update base url

--- packages/snippets/vite.config.ts
+++ packages/snippets/vite.config.ts
@@ -1,18 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: 'https://cdn.jsdelivr.net/gh/aimlapi/api-docs/snippet-app/dist',
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 'src/main.ts',
-      output: {
-        entryFileNames: 'bundle.js',
-        assetFileNames: 'bundle.css',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: env.SNIPPETS_CDN_PREFIX,
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: 'src/main.ts',
+        output: {
+          entryFileNames: 'bundle.js',
+          assetFileNames: 'bundle.css',
+        },
       },
     },
-  },
-  assetsInclude: ['**/*.hbs'],
+    assetsInclude: ['**/*.hbs'],
+  };
 });
