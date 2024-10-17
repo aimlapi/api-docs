b529458bc10505b3811f6c793dc4cc8628595bef
Daniil Bombenkov <danixoon@gmail.com>
2024-10-17 14:45:15 +0300

    fix: update styles

--- packages/snippets/src/components/snippet-viewer/content/index.tsx
+++ packages/snippets/src/components/snippet-viewer/content/index.tsx
@@ -28,7 +28,7 @@ const Content: FC<Props> = (props) => {
     return [];
   }, [loading, option.content]);
 
-  const scrollbar = useMemo<ScrollbarProps>(() => ({ autoHeight: true, autoHeightMax: 492 }), []);
+  const scrollbar = useMemo<ScrollbarProps>(() => ({ autoHeight: true, autoHeightMax: 432 }), []);
 
   return (
     <>
