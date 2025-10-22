61bfd97d6f8369b54584c5884a3634b03dae1212
Andrei <titov680andrei@gmail.com>
2025-10-22 14:03:55 +0400

    feat: add code samples

---
+++ packages/generators-v2/src/code-samples/custom.sample.js
@@ -0,0 +1,92 @@
+export const customSample = (options) => {
+  const samples = [
+    {
+      lang: 'JavaScript',
+      source: `async function main() {
+  const response = await fetch('https://api.aimlapi.com/v1/images/generations', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer <YOUR_AIMLAPI_KEY>',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      model: '${options.name}',
+      prompt: 'A T-Rex relaxing on a beach, lying on a sun lounger and wearing sunglasses.',
+    }),
+  });
+
+  const data = await response.json();
+  console.log(JSON.stringify(data, null, 2));
+}
+
+main();`,
+    },
+    {
+      lang: 'Python',
+      source: `import requests
+
+response = requests.post(
+    "https://api.aimlapi.com/v1/images/generations",
+    headers={
+        "Content-Type":"application/json", 
+        "Authorization":"Bearer <YOUR_AIMLAPI_KEY>",
+    },
+    json={
+        "model":"${options.name}",
+        "prompt": "A T-Rex relaxing on a beach, lying on a sun lounger and wearing sunglasses."
+    }
+)
+
+data = response.json()
+print(data)`,
+    },
+    {
+      lang: 'cURL',
+      source: `curl -L \\
+  --request POST \\
+  --url 'https://api.aimlapi.com/v1/images/generations' \\
+  --header 'Authorization: Bearer <YOUR_AIMLAPI_KEY>' \\
+  --header 'Content-Type: application/json' \\
+  --data '{
+    "model": "${options.name}",
+    "prompt": "A T-Rex relaxing on a beach, lying on a sun lounger and wearing sunglasses."
+  }'`,
+    },
+    {
+      lang: 'HTTP',
+      source: `POST /v1/images/generations HTTP/1.1
+Host: api.aimlapi.com
+Authorization: Bearer <YOUR_AIMLAPI_KEY>
+Content-Type: application/json
+Accept: */*
+
+{
+  "model": "${options.name}",
+  "prompt": "A T-Rex relaxing on a beach, lying on a sun lounger and wearing sunglasses."
+}`,
+    },
+  ];
+  Object.entries(options.customParams).forEach(([paramName, paramValue]) => {
+    samples.push({
+      lang: 'JavaScript',
+      source: `async function main() {
+  const response = await fetch('https://api.aimlapi.com/v1/images/generations', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer <YOUR_AIMLAPI_KEY>',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      model: '${options.name}',
+      prompt: 'A T-Rex relaxing on a beach, lying on a sun lounger and wearing sunglasses.',
+    }),
+  });
+
+  const data = await response.json();
+  console.log(JSON.stringify(data, null, 2));
+}
+
+main();`,
+    });
+  });
+};
---
+++ packages/generators-v2/src/code-samples/index.js
@@ -0,0 +1,5 @@
+export * from './chat-completion.sample.js';
+export * from './chat-completion-audio.sample.js';
+export * from './text-to-image.sample.js';
+export * from './image-to-image.sample.js';
+export * from './gpt-image-edit.sample.js';
