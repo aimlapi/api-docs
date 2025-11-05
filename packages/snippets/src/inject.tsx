901e63cee1595f716693f7fe5cc2044d7a1eaa04
Daniil Bombenkov <danixoon@gmail.com>
2025-11-05 13:06:50 +0300

    fix: injector polling

--- packages/snippets/src/inject.tsx
+++ packages/snippets/src/inject.tsx
@@ -13,24 +13,41 @@ export const inject = () => {
       <App />
     </StrictMode>
   );
+  console.log("snippets injected");
 
   injectButtons();
 };
 
 export const injectButtons = () => {
   try {
-    const buttons = document.querySelectorAll(".api-main-buttom.w-button");
-    buttons.forEach((b) => {
-      const url = new URL(b.getAttribute("href")!);
-      const model = window.location.pathname.match(/\/models\/(.+?)\/?/)?.[1];
-      if (model) {
-        url.searchParams.set("model", model);
+    let attempts = 0;
+    const intervalId = setInterval(() => {
+      const buttons = document.querySelectorAll(".api-main-buttom.w-button");
+      buttons.forEach((b) => {
+        const url = new URL(b.getAttribute("href")!);
+        const model = window.location.pathname.match(/\/models\/(.+?)\/?/)?.[1];
+        if (model) {
+          url.searchParams.set("model", model);
+        }
+
+        url.hostname = window.location.hostname;
+
+        b.setAttribute("href", url.toString());
+      });
+
+      if (attempts > 20) {
+        console.error("failed to inject hostnames: timeout exceeded");
+        clearInterval(intervalId);
       }
 
-      url.hostname = window.location.hostname;
-
-      b.setAttribute("href", url.toString());
-    });
+      if (buttons.length > 0) {
+        console.error("hostnames injected");
+        clearInterval(intervalId);
+      } else {
+        console.warn("failed to inject hostnames: no buttons detected");
+        attempts += 1;
+      }
+    }, 1000);
   } catch (error) {
     console.error("failed to update hostnames", error);
   }
