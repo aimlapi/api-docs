79925a7c0d2343fe274e7e5762c6124a3937079d
Andrei <titov680andrei@gmail.com>
2025-11-07 19:34:40 +0400

    feat: add descs for video response

---
+++ packages/openapi/descriptions/video.response.js
@@ -0,0 +1,23 @@
+export default {
+  id: {
+    desc: `The ID of the generated video.`,
+  },
+  status: {
+    desc: `The current status of the generation task.`,
+  },
+  url: {
+    desc: `The URL where the file can be downloaded from.`,
+  },
+  duration: {
+    desc: `The duration of the video.`,
+  },
+  error: {
+    desc: `Description of the error, if any.`,
+  },
+  meta: {
+    desc: `Additional details about the generation.`,
+  },
+  tokens_used: {
+    desc: `The number of tokens consumed during generation.`,
+  },
+};
