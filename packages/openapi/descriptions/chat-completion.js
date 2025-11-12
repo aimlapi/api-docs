97bdeb193d31332b15485c0d4130fbd0a4b1378b
Slava Telyatko <145783276+stelyatko@users.noreply.github.com>
2025-11-12 09:38:46 +0300

    fix: update modalities description for completions models to improve clarity

--- packages/openapi/descriptions/chat-completion.js
+++ packages/openapi/descriptions/chat-completion.js
@@ -355,7 +355,7 @@ export default {
   
   ["text"]
   
-  The gpt-4o-audio-preview model can also be used to generate audio. To request that this model generate both text and audio responses, you can use:
+  Model can also be used to generate audio. To request that this model generate both text and audio responses, you can use:
   
   ["text", "audio"]`,
   },
