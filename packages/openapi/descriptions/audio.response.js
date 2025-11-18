0404386baf016b245f7410e0f38bfe1458379188
Andrei <titov680andrei@gmail.com>
2025-11-18 19:11:16 +0400

    feat: add descs for audio models

---
+++ packages/openapi/descriptions/audio.response.js
@@ -0,0 +1,20 @@
+export default {
+  id: {
+    desc: `The ID of the generated audio.`,
+  },
+  status: {
+    desc: `The current status of the generation task.`,
+  },
+  url: {
+    desc: `The URL where the file can be downloaded from.`,
+  },
+  error: {
+    desc: `Description of the error, if any.`,
+  },
+  meta: {
+    desc: `Additional details about the generation.`,
+  },
+  tokens_used: {
+    desc: `The number of tokens consumed during generation.`,
+  },
+};
