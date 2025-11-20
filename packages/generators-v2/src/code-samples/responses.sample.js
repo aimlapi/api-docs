0b89ed4574ce6a1fcf48eb3a35dd46a6d46f193c
Andrei <titov680andrei@gmail.com>
2025-11-20 16:47:36 +0400

    feat: add responses snippets

---
+++ packages/generators-v2/src/code-samples/responses.sample.js
@@ -0,0 +1,63 @@
+export const responsesSample = (options) => {
+  const params = {
+    model: options.name,
+    input: 'Hello',
+  };
+  const jsonBody = JSON.stringify(params, null, 2);
+
+  return [
+    {
+      lang: 'JavaScript',
+      source: `async function main() {
+  const response = await fetch('https://api.aimlapi.com/v1/responses', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer <YOUR_API_KEY>',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(${jsonBody.replace(/\n/g, '\n    ')}),
+  });
+
+  const data = await response.json();
+  console.log(JSON.stringify(data, null, 2));
+}
+
+main();`,
+    },
+    {
+      lang: 'Python',
+      source: `import requests
+
+response = requests.post(
+    "https://api.aimlapi.com/v1/responses",
+    headers={
+        "Content-Type": "application/json",
+        "Authorization": "Bearer <YOUR_API_KEY>",
+    },
+    json=${jsonBody.replace(/\n/g, '\n    ')}
+)
+
+data = response.json()
+print(data)`,
+    },
+    {
+      lang: 'cURL',
+      source: `curl -L \\
+  --request POST \\
+  --url 'https://api.aimlapi.com/v1/responses' \\
+  --header 'Authorization: Bearer <YOUR_API_KEY>' \\
+  --header 'Content-Type: application/json' \\
+  --data '${jsonBody.replace(/'/g, "\\'").replace(/\n/g, '\n    ')}'`,
+    },
+    {
+      lang: 'HTTP',
+      source: `POST / HTTP/1.1
+Host: test.com
+Authorization: Bearer <YOUR_API_KEY>
+Content-Type: application/json
+Accept: */*
+
+${jsonBody}`,
+    },
+  ];
+};
