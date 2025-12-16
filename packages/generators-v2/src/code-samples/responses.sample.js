1dc8b661f7f145e7472aff045c0fbcd41b3a7120
Andrei <titov680andrei@gmail.com>
2025-12-16 12:02:45 +0400

    fix: update code sample for http

--- packages/generators-v2/src/code-samples/responses.sample.js
+++ packages/generators-v2/src/code-samples/responses.sample.js
@@ -51,8 +51,8 @@ print(data)`,
     },
     {
       lang: 'HTTP',
-      source: `POST / HTTP/1.1
-Host: test.com
+      source: `POST /v1/responses HTTP/1.1
+Host: api.aimlapi.com
 Authorization: Bearer <YOUR_AIMLAPI_KEY>
 Content-Type: application/json
 Accept: */*
